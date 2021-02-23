1e4f3a69b2cb4c0cec5f6b6a27fb2ab81aff5940
Mario Leandro <marioleandro34@gmail.com>
2021-02-23 20:06:23 -0300

    refactor: migrate to Next.JS

---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import styles from '../styles/pages/Home.module.css';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar/>
+      <section>
+        <div>
+          <Profile/>
+          <CompletedChallenges/>
+          <Countdown/>
+        </div>
+        <div></div>
+      </section>
+    </div>
+  )
+}
