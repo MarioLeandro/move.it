69a878a72cd2f5f31deaff79518b4597dfbb8276
Mario Leandro <marioleandro34@gmail.com>
2021-02-24 20:01:33 -0300

    feat: add contexts

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown () {
-    const [time, setTime] = useState(0.5 * 60);
+    const {startNewChallenge} = useContext(ChallengesContext);
+
+    const [time, setTime] = useState(0.1 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
@@ -32,6 +35,7 @@ export function Countdown () {
         } else if (isActive && time == 0) {
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
