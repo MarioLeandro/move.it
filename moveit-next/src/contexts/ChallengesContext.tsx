effe6090d9d7c305192f52df7de49af9c21d9a7d
Mario Leandro <marioleandro34@gmail.com>
2021-02-25 22:23:06 -0300

    feat: add notification

--- moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useState} from 'react';
+import {createContext, ReactNode, useEffect, useState} from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge {
@@ -32,6 +32,10 @@ export function ChallengesProvider ({children} : ChallengesProviderProps) {
     const[activeChallenge, setActiveChallenge] = useState(null);
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+    useEffect(() => {
+        Notification.requestPermission();
+    }, [])
+
     function levelUp () {
         setLevel(level + 1);
     }
@@ -40,6 +44,14 @@ export function ChallengesProvider ({children} : ChallengesProviderProps) {
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
         const challenge = challenges[randomChallengeIndex];
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        if (Notification.permission == 'granted') {
+            new Notification('Novo desafio 🎉', {
+             body: `Valendo ${challenge.amount} xp!`   
+            });
+        }
     }
 
     function resetChallenge () {
